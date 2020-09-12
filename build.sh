a8a6500727130051523826c509d0a969761661ea
Dong-Young Kim <31337.persona@gmail.com>
2020-09-12 20:08:23 +0900

    fix: fix build script

--- build.sh
+++ build.sh
@@ -7,26 +7,27 @@ DIR_SUBSET="${DIR}/subset"
 for filepath in "${DIR_ORIGIN}"/*; do
   filename="$(basename "${filepath}")"
   filename="${filename//-*[0-9]/}"
+  printf "\n${filename}...\n"
 
   for ext in ttf woff woff2; do
     output="${DIR_SUBSET}/${filename/ttf/$ext}"
-    glyphs="${DIR_SUBSET}/glyphs.txt"
-    args=" ${filepath} --output-file=${output} --text-file=${glyphs} \
-      --layout-features='*' --glyph-names --symbol-cmap --legacy-cmap \
-      --notdef-glyph --notdef-outline --recommended-glyphs \
-      --name-IDs='*' --name-legacy --name-languages='*' \
-      --drop-tables="
+    args="${filepath} --output-file=${output}"
 
+    echo "${filename} to ${output##.*/}"
     case "${ext}" in
       woff)
         args+=" --with-zopfli"
         ;&
       woff2)
         args+=" --flavor=${ext}"
-        ;&
-      *) ;;
+        ;;
     esac
 
-    pyftsubset ${args}
+  pyftsubset ${args} \
+    --text-file=${DIR_SUBSET}/glyphs.txt --unicodes-file=${DIR_SUBSET}/unicodes.txt \
+    --layout-features='*' --glyph-names --symbol-cmap --legacy-cmap \
+    --notdef-glyph --notdef-outline --recommended-glyphs \
+    --name-IDs='*' --name-legacy --name-languages='*' \
+    --drop-tables=
   done
 done
